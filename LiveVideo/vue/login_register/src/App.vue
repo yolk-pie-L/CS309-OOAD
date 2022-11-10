93bbdb06692dadf74fb353e6fd8aae7f1638f171
陈睿瑶 <chenruiyao@chenruiyaodeMacBook-Pro.local>
2022-11-10 20:18:58 +0800

    homepage

---
+++ LiveVideo/vue/login_register/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/regist">Regist</router-link>
+    </nav>
+
+    <!--路由页面-->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+/*全局的父类高度*/
+html {
+  height: 100%;
+}
+
+/*重置body属性*/
+body {
+  padding: 0;
+  margin: 0;
+  /*继承html*/
+  height: 100%;
+}
+
+/*#app的高度也需要继承*/
+#app {
+  height: 100%;
+}
+
+/*全局链接标签去下划线*/
+a {
+  text-decoration: none;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+</style>
