ebb3653275800a46a4020f2140975456ab3c4bf1
blackwang08 <1580716052@qq.com>
2022-12-11 17:43:48 +0800

    实现报名退出

--- LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/views/Login.vue
+++ LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/views/Login.vue
@@ -92,7 +92,7 @@ export default {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           // 表单验证成功
-          this.$axios.post('http://localhost:8082/user/login', this.loginForm).then(res => {
+          this.$axios.post('http://localhost:8082/api/user/login', this.loginForm).then(res => {
             // 拿到结果
             let result = JSON.parse(res.data.data);
             let message = res.data.msg;
