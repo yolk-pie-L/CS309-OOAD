3df8f21f001f9df49c03b9980958f238414a7770
chenrrry <wonu19960717@163,com>
2022-12-18 16:12:03 +0800

    remove homeworkPage beta

--- LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/router/index.js
+++ LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/router/index.js
@@ -129,22 +129,22 @@ const router = createRouter({
 })
 
 
-// 导航守卫
-// 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
-router.beforeEach((to, from, next) => {
-//如果去往登录页则放行
-  if (to.path === '/login'|| to.path ==='/' ||to.path==='/register'||to.path==='/administer'||to.path==='/teacher') {
-    next();
-  } else {
-    // 从本地存储里获取token
-    let token = localStorage.getItem('token');
-    // 判断token是否为空如果为空则跳转到登录页 如果有则放行
-    if (token === null || token === '') {
-      next({path:'/'});
-    } else {
-      next();
-    }
-  }
-});
+// // 导航守卫
+// // 使用 router.beforeEach 注册一个全局前置守卫，判断用户是否登陆
+// router.beforeEach((to, from, next) => {
+// //如果去往登录页则放行
+//   if (to.path === '/login'|| to.path ==='/' ||to.path==='/register'||to.path==='/administer'||to.path==='/teacher') {
+//     next();
+//   } else {
+//     // 从本地存储里获取token
+//     let token = localStorage.getItem('token');
+//     // 判断token是否为空如果为空则跳转到登录页 如果有则放行
+//     if (token === null || token === '') {
+//       next({path:'/'});
+//     } else {
+//       next();
+//     }
+//   }
+// });
 
 export default router
--- LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/views/HomeworkPage.vue
+++ LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/views/HomeworkPage.vue
@@ -1,200 +1,101 @@
 <template>
-  <div>
-    <div>
-      <el-row style="height: 95vh">
-        <el-col :span="24" style="height: 100%">
-          <el-card class="welcome" shadow=hover>
-            <el-header class="tit" v-text="homeworkForm.courseName "></el-header>
-            <el-header class="tit" v-text="homeworkForm.assignmentName "></el-header>
-            <div class="border"></div>
-          </el-card>
-        </el-col>
-      </el-row>
-    </div>
-
-    <div class="describe">
-      <el-row>
-        <el-header class="variable1">AssignmentName:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.assignmentName "></el-header>
-      </el-row>
-      <el-row>
-        <el-header class="variable1">Deadline:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.deadline "></el-header>
-      </el-row>
-      <el-row>
-        <el-header class="variable1">Status:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.status "></el-header>
-      </el-row>
-      <el-row>
-        <el-header class="variable1">Score:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.score "></el-header>
-        <el-header class="variable2">/</el-header>
-        <el-header class="variable2" v-text="homeworkForm.totalGrade "></el-header>
-      </el-row>
-      <el-row>
-        <el-header class="variable1">Create Time:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.createTime "></el-header>
-      </el-row>
-      <el-row>
-        <el-header class="variable1">Update Time:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.updateTime "></el-header>
-      </el-row>
-      <el-row>
-        <el-header class="variable1">Description:</el-header>
-        <el-header class="variable2" v-text="homeworkForm.description "></el-header>
-      </el-row>
-    </div>
-
-    <div class="add">
-      <el-header class="variable1">AdditionalSource:</el-header>
-      <div v-for="item in additionalResources">
-        <el-link
-            :body-style="{ padding: '0px', marginBottom: '1px' }"
-            :href="item.resourceUrl"
-            class="addi"
-            v-text="item.resourceName">
-        </el-link>
-      </div>
-    </div>
-
-<!--    <div class="answer">-->
-<!--      <el-header class="variable1">Answer:</el-header>-->
-<!--      <div v-for="item in answer">-->
-<!--        <el-link-->
-<!--            :body-style="{ padding: '0px', marginBottom: '1px' }"-->
-<!--            :href="item.answerUrl"-->
-<!--            class="addi"-->
-<!--            v-text="item.answerName">-->
-<!--        </el-link>-->
-<!--      </div>-->
-<!--    </div>-->
-
-<!--    <div class="upload">-->
-<!--      <el-upload ref="upload" :before-upload="beforeUpload" :file-list="list" :limit="1" action="doUpload"-->
-<!--                 class="upload-demo">-->
-<!--        <el-button slot="trigger" size="small" type="primary">选取文件</el-button>-->
-<!--        <div slot="tip" class="el-upload__tip">文件大小不能不超过5mb</div>-->
-<!--        <div slot="tip" class="el-upload-list__item-name">{{list}}</div>-->
-<!--      </el-upload>-->
-<!--      <span slot="footer" class="dialog-footer">-->
-<!--        <el-button @click="visible = false">取消</el-button>-->
-<!--        <el-button type="primary" @click="submitUpload()">确定</el-button>-->
-<!--      </span>-->
-<!--    </div>-->
-
-<!--    <el-upload-->
-<!--        class="upload-demo"-->
-<!--        action="https://jsonplaceholder.typicode.com/posts/"-->
-<!--        :on-preview="handlePreview"-->
-<!--        :on-remove="handleRemove"-->
-<!--        :before-remove="beforeRemove"-->
-<!--        multiple-->
-<!--        :limit="3"-->
-<!--        :on-exceed="handleExceed"-->
-<!--        :file-list="fileList">-->
-<!--      <el-button size="small" type="primary">点击上传</el-button>-->
-<!--      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>-->
-<!--    </el-upload>-->
-    <el-row type="flex" class="row-bg card" justify="center" align="bottom">
+  <div :xl="6" :lg="7" class="bg-login">
+    <!--logo-->
+    <!--标题-->
+    <el-row style="height: 95vh">
+      <el-col :span="24" style="height: 100%">
+        <el-card shadow=hover class="welcome">
+          <el-header class="tit" v-text="classForm.courseName" ></el-header>
+          <el-header class="tit" >Upload Assignment</el-header>
+          <div class="border"></div>
+        </el-card>
+      </el-col>
+    </el-row>
+    <!--form表单-->
+    <el-row type="flex" class="row-bg" justify="center" align="bottom">
       <el-col :span="7" class="login-card">
         <!--loginForm-->
-        <el-form :model="sectionForm" :rules="rules" ref="loginForm" label-width="21%" class="loginForm">
-          <el-form-item label="章节名称" prop="sectionName" style="width: 380px">
-            <el-input v-model="sectionForm.sectionName"></el-input>
+        <el-form :model="homeworkForm" :rules="rules" ref="loginForm" label-width="21%" class="loginForm">
+          <el-form-item label="AssignmentName"  prop="assignmentName" style="width: 380px">
+            <el-input v-model="homeworkForm.assignmentName"></el-input>
           </el-form-item>
-          <el-upload
-              action="#"
-              multiple
-              :auto-upload="false"
-              :show-file-list="true"
-              :on-change="handleChange"
-              drag>
-            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-            <div class="el-upload__text">
-              将文件拖到此处，或<em>点击上传</em>
-            </div>
-          </el-upload>
+          <el-form-item label="Deadline" prop="deadline" style="width: 380px">
+            <el-input v-model="homeworkForm.deadline"></el-input>
+          </el-form-item>
+          <el-form-item label="Description" prop="description" style="width: 380px">
+            <el-input v-model="homeworkForm.description"></el-input>
+          </el-form-item>
+          <el-form-item label="totalGrade" prop="totalGrade" style="width: 380px">
+            <el-input v-model="homeworkForm.totalGrade"></el-input>
+          </el-form-item>
+          <el-form-item label="Additional Source" prop="additionalResources" style="width: 380px">
+            <el-upload
+                :action="uploadURL"
+                :style="{backgroundImage:'url(' + dialogImageUrl + ')', backgroundRepeat:'no-repeat', backgroundPosition:'center center', backgroundSize: 'contain'}"
+                list-type="picture-card"
+                class="uploadImg"
+                name="files"
+                :before-upload="beforeUpload">
+              <i class="el-icon-plus"></i>
+            </el-upload>
+          </el-form-item>
           <el-form-item class="btn-ground">
-            <el-button type="primary" @click="submitForm('loginForm')">Update</el-button>
-            <el-button @click="resetForm('loginForm')">Reset</el-button>
+            <el-button type="primary" @click="submitForm('loginForm')">立即上传</el-button>
+            <el-button @click="resetForm('loginForm')">重置</el-button>
           </el-form-item>
         </el-form>
       </el-col>
     </el-row>
-
-
   </div>
 </template>
 
 <script>
-
 import router from "@/router";
 
 export default {
-  name: "Homework",
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "HomeworkUpload",
   data() {
     return {
-      homeworkForm: {
-        assignmentId: "aa",
+      classForm: {
+        id: "",
         courseName: "course",
-        teacherName: "teacher",
-        assignmentName: "homework1",
-        deadline: "date",
-        description: "Intro",
-        status: "notStarted/return/submitted",
-        score: "",
+        teacherName: "",
+        introduction: "",
+        charge: "",
+        tag: "",
+        coursePicture: "",
+        privateKeyUrl: "",
+        status: ""
+      },
+      homeworkForm: {
+        courseId:"",
+        assignmentName: "",
+        deadline: "",
+        description: "",
         totalGrade: "",
-        createTime: "date",
-        updateTime: "date",
-        answerStatus: "yes/no",
+        additionalResources: [
+          {
+            resourceName: "",
+            resourceUrl: ""
+          }
+        ]
       },
-      additionalResources: [
-        {
-          resourceName: "aa",
-          resourceUrl: "https://element.eleme.io"
-        },
-        {
-          resourceName: "aa",
-          resourceUrl: "https://element.eleme.io"
-        }
-      ],
-      answers: [
-        {
-          answerName: "a",
-          answerUrl: "b"
-        },
-        {
-          answerName: "c",
-          answerUrl: "d"
-        }
-      ],
-      uploadAnswer:{
-        file:"",
+      rules: {
+
       },
-      fileList: [
-        {
-          name: 'food.jpeg',
-          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-        },
-        {
-          name: 'food2.jpeg',
-          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-        }
-      ],
-    }
+    };
   },
   mounted() {
     this.fetchAssignment()
   },
   methods: {
-    fetchAssignment() {
-      this.$axios.get('api/course/assignment?AssignmentId={' + this.$router.query.id + '}').then(res => {
+    fetchClass() {
+      this.$axios.get('api/course/{' + this.classForm.id + '}').then(res => {
         // 拿到结果
         let result = JSON.parse(res.data.data);
         let message = res.data.msg;
-        this.homeworkForm = result
-        this.additionalResources = result.additionalResources
-        this.answer = result.answer
+        this.classForm=result
         // 判断结果
         if (result) {
         } else {
@@ -203,36 +104,28 @@ export default {
         }
       })
     },
-    uploading() {
-      this.$message({
-        message: '请等待上传完成',
-        type: 'error'
-      })
+    beforeUpload (file) {
+      this.homeworkForm.additionalResources.append('file', file)
+      return false
     },
-    handleChange(file, fileList) {
-      let formdata = new FormData()
-      fileList.map(item => { //fileList本来就是数组，就不用转为真数组了
-        formdata.append("file", item.raw)  //将每一个文件图片都加进formdata
-      })
-      console.log(file.size)
-      this.$axios.post("http://localhost:8082/api/getPhoto", formdata).then(res => {
-        console.log(res)
-      })
-    },
+    // 提交表单
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
           // 表单验证成功
-          this.$axios.post('http://localhost:8082/api/upload', this.sectionForm).then(res => {
+          this.$axios.post('http://localhost:8082/user/checkLogin', this.courseForm).then(res => {
             // 拿到结果
             let result = JSON.parse(res.data.data);
             let message = res.data.msg;
             // 判断结果
             if (result) {
               /*登陆成功*/
+              Element.Message.success(message);
+              /*跳转页面*/
               router.push('/')
             } else {
-
+              /*打印错误信息*/
+              Element.Message.error(message);
             }
           })
         } else {
@@ -249,42 +142,27 @@ export default {
 }
 </script>
 
-<style>
-.tableA th,
-.tableA tr,
-.tableA td {
-  background-color: transparent;
-}
-
-.tableA {
-  background-color: transparent;
-  position: absolute;
-  top: 35%;
-  left: 50%;
-  width: 30%;
-  border-radius: 10%;
-  font-size: 20px;
-  font-weight: 600;
-
-}
-
-</style>
-
 <style scoped>
 
-.variable1 {
-  display: inline-block;
-  position: center;
-  top: 5px;
-  font-size: 20px;
-  font-weight: 600;
+.bg-login {
+  height: 100%;
+  background-size: 200%;
+
 }
 
-.variable2 {
-  display: inline-block;
-  position: center;
-  font-size: 18px;
-  font-weight: 500;
+.btn-ground {
+  text-align: center;
+}
+
+.login-card {
+  background-color: #ffffff;
+  opacity: 0.9;
+  box-shadow: 0 0 20px #ffffff;
+  border-radius: 10px;
+  padding: 40px 40px 30px 15px;
+  width: auto;
+  position: absolute;
+  left: 70%;
 }
 
 /deep/ .welcome {
@@ -300,48 +178,9 @@ export default {
   font-weight: 600;
 }
 
-.tit {
-  position: center;
-  top: 5px;
-}
-
-.describe {
+.row-bg{
   position: absolute;
-  background-color: #c1d1d7;
-  top: 40%;
-  left: 10%;
-  border-radius: 10%;
-}
-
-.add {
-  position: absolute;
-  top: 30%;
-  left: 50%;
-  width: 40%;
-  height: 20%;
-  background-color: #c1d1d7;
-}
-
-.addi {
-  font-size: 30px;
-  font-weight: 600;
-}
-
-.answer {
-  position: absolute;
-  top: 52%;
-  left: 50%;
-  width: 40%;
-  height: 20%;
-  background-color: #c1d1d7;
-}
-
-.upload {
-  position: absolute;
-  top: 75%;
-  left: 50%;
-  width: 40%;
-  height: 20%;
-  background-color: #c1d1d7;
+  top: 20%;
+  left: 70%;
 }
 </style>
