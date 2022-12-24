57352fe1e4d5c36860bb09c22e6e28a29afca43e
yolk-pie-L <493577282@qq.com>
2022-12-24 16:31:34 +0800

    remove Comment Constructor

--- LiveVideo/src/main/java/com/example/live_video/dto/CommentForm.java
+++ LiveVideo/src/main/java/com/example/live_video/dto/CommentForm.java
@@ -1,10 +1,8 @@
 package com.example.live_video.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class CommentForm {
     Long sectionId;
     String userName;
