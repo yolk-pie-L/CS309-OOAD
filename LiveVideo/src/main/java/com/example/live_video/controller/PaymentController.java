c0cd109a3893c3c88eeb2bedadb7e3572f9f70fd
throusea <1353272517@qq.com>
2023-01-02 02:27:32 +0800

    支付信息显示，修改支付路径

--- LiveVideo/src/main/java/com/example/live_video/controller/PaymentController.java
+++ LiveVideo/src/main/java/com/example/live_video/controller/PaymentController.java
@@ -10,7 +10,10 @@ import com.example.live_video.wrapper.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -30,11 +33,54 @@ public class PaymentController {
         return new StringVo(paymentService.toAlipay(alipayBean));
     }
 
+    @GetMapping("/api/alipay/notify")
+    public StringVo notify(HttpServletRequest request) {
+        if (request.getParameter("trade_status").equals("TRADE_SUCCESS")) {
+            System.out.println("=========支付宝异步回调========");
+            Map<String, String> params = new HashMap<>();
+            Map<String, String[]> requestParams = request.getParameterMap();
+            for (String name : requestParams.keySet()) {
+                params.put(name, request.getParameter(name));
+                // System.out.println(name + " = " + request.getParameter(name));
+            }
+            String tradeNo = params.get("out_trade_no");
+            String gmtPayment = params.get("gmt_payment");
+            String alipayTradeNo = params.get("trade_no");
+            System.out.println("交易名称: " + params.get("subject"));
+            System.out.println("交易状态: " + params.get("trade_status"));
+            System.out.println("支付宝交易凭证号: " + params.get("trade_no"));
+            System.out.println("商户订单号: " + params.get("out_trade_no"));
+            System.out.println("交易金额: " + params.get("total_amount"));
+            System.out.println("买家在支付宝唯一id: " + params.get("buyer_id"));
+            System.out.println("买家付款时间: " + params.get("gmt_payment"));
+            System.out.println("买家付款金额: " + params.get("buyer_pay_amount"));
+            System.out.println("Sign: " + params.get("sign"));
+            // 更新订单为已支付(4)
+        }
+        return new StringVo("通知成功");
+    }
+
     @GetMapping("/api/alipay/success")
-    public StringVo success(@RequestParam(value = "out_trade_no") String tradeNo,
-                            @RequestParam(value = "total_amount") String totalAmount) {
-        System.out.println(tradeNo);
-        System.out.println(totalAmount);
+    public StringVo success(HttpServletRequest request) {
+        System.out.println("=========支付宝异步回调========");
+        Map<String, String> params = new HashMap<>();
+        Map<String, String[]> requestParams = request.getParameterMap();
+        for (String name : requestParams.keySet()) {
+            params.put(name, request.getParameter(name));
+            // System.out.println(name + " = " + request.getParameter(name));
+        }
+        String tradeNo = params.get("out_trade_no");
+        String gmtPayment = params.get("gmt_payment");
+        String alipayTradeNo = params.get("trade_no");
+        System.out.println("交易名称: " + params.get("subject"));
+        System.out.println("交易状态: " + params.get("trade_status"));
+        System.out.println("支付宝交易凭证号: " + params.get("trade_no"));
+        System.out.println("商户订单号: " + params.get("out_trade_no"));
+        System.out.println("交易金额: " + params.get("total_amount"));
+        System.out.println("买家在支付宝唯一id: " + params.get("buyer_id"));
+        System.out.println("买家付款时间: " + params.get("gmt_payment"));
+        System.out.println("买家付款金额: " + params.get("buyer_pay_amount"));
+        // 更新订单为已支付(4)
         return new StringVo("交易成功");
     }
 }
