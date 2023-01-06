ba37618d2947230a51278cc0fe03788707aae4fe
chenrrry <wonu19960717@163,com>
2023-01-07 00:01:12 +0800

    motify courseMainPage.vue

--- LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/views/courseMainPage.vue
+++ LiveVideo/vue/VUE_HOMEPAGE_LOGIN_REGISTER/src/views/courseMainPage.vue
@@ -1,25 +1,30 @@
 <template>
   <div :xl="6" :lg="7" class="home">
     <!--标题-->
-    <el-row type="flex" class="row-bg row-two" justify="center" align="top">
-      <el-col :span="10">
-        <!--标题-->
-        <img src="@/assets/logo.png" alt="logo">
-      </el-col>
-      <el-col :span="10">
-        <el-row v-text="this.courseForm.courseName" class="title"></el-row>
-        <el-row v-text="this.courseForm.teacherName" class="title"></el-row>
-        <el-row v-text="this.courseForm.charge" class="title"></el-row>
-      </el-col>
-    </el-row>
-    <el-row v-show="coShow" type="flex" class="row-bg card" justify="center" align="bottom">
-      <el-button type="primary" @click="detailed">课程信息</el-button>
-    </el-row>
-    <el-row v-show="coNotShow" type="flex" class="row-bg card" justify="center" align="bottom">
-      <el-button type="primary" @click="detailed">课程信息</el-button>
-      <el-button type="primary" @click="joinCourse">报名课程</el-button>
-      <el-button type="primary" @click="leave">退课</el-button>
-    </el-row>
+    <div :style="{'background-image': bgUrl}" class="bgBackground">
+      <el-row type="flex" class="row-bg row-two" justify="center" align="top" >
+        <el-col :span="10">
+          <!--标题-->
+          <img src="https://i.328888.xyz/2023/01/06/kll8N.jpeg" class="img1">
+        </el-col>
+        <el-col :span="10">
+          <el-row v-text="this.courseForm.courseName" class="variable1"></el-row>
+          <el-row v-text="this.courseForm.teacherName" class="variable2"></el-row>
+          <el-row  class="variable3">¥</el-row>
+          <el-row v-text="this.courseForm.charge" class="variable3"></el-row>
+        </el-col>
+      </el-row>
+    </div>
+    <div :style="{'background-image': bgUrl1}" class="bgBackground1">
+      <el-row v-show="coShow" type="flex" class="type2" justify="center" align="bottom" >
+        <el-button type="primary" @click="detailed" class="variable4">课程信息</el-button>
+      </el-row>
+      <el-row v-show="coNotShow" type="flex" class="type1" justify="center" align="bottom">
+        <el-button type="primary" @click="detailed" class="variable4">课程信息</el-button>
+        <el-button type="primary" @click="joinCourse" class="variable4">报名课程</el-button>
+        <el-button type="primary" @click="leave" class="variable4">退课</el-button>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -37,14 +42,17 @@ export default {
       courseId: useRoute().query.courseId,
       courseForm: {
         courseName: "DSAA",
-        teacherName: "李开",
+        teacherName: "CHEN",
         coursePic: "url",
         charge: 0
       },
       joinForm: {
         studentName: 'black',
         courseId: ''
-      }
+      },
+      bgUrl:'url(https://i.328888.xyz/2023/01/06/kCf0z.jpeg)',
+      bgUrl1:'url(https://i.328888.xyz/2023/01/06/klJmJ.jpeg)',
+
     }
   },
   mounted() {
@@ -163,5 +171,88 @@ export default {
   margin-bottom: 20px;
   color: black;
   font-size: 40px;
+}
+
+.bgBackground{
+  background-repeat:no-repeat;
+  background-size: 100%;
+  width:100%;
+  height:30%;
+  opacity: 0.75;
+}
+
+.bgBackground1{
+  background-repeat:no-repeat;
+  background-size: 100%;
+  position: absolute;
+  top:43%;
+  width:100%;
+  height:60%;
+  opacity: 0.8;
+}
+
+.img1{
+  position: absolute;
+  top:10%;
+  left: 20%;
+  width: 250px;
+  height: 250px;
+  border-radius: 20px;
+  border: 2px solid #000000;
+}
+
+.variable1 {
+  display: inline-block;
+  position: center;
+  top: 5px;
+  width: 80%;
+  font-size: 100px;
+  font-weight: 600;
+  font-family: mine;
+}
+
+.variable2 {
+  display: inline-block;
+  position: center;
+  width: 100%;
+  font-size: 50px;
+  font-weight: 500;
+  font-family: dia;
+}
+
+.variable3 {
+  display: inline-block;
+  position: center;
+  font-size: 50px;
+  font-weight: 500;
+  font-family: dia;
+}
+
+.variable4 {
+  position: center;
+  font-size: 30px;
+  font-weight: 400;
+  font-family: dia;
+}
+
+.type1{
+  position: center;
+  width: 30%;
+  height: 9%;
+  top: 55%;
+  left: 40%;
+  border-radius: 2px;
+  border: 2px solid #000000;
+}
+
+.type2{
+  position: center;
+  font-family: dfx;
+  width: 12%;
+  height: 9%;
+  top: 55%;
+  left: 45%;
+  border-radius: 2px;
+  border: 2px solid #000000;
 }
 </style>
